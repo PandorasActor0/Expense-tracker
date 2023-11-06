5d7e155c0aef2ee752c1b9367d8e9352de38f728
PandorasActor0 <nelsona2001@hotmail.com>
2023-11-05 20:37:24 -0500

    Add file config.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from config import config
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Welcome"
+
+
 if __name__=='__main__':
+    app.config.from_object(config['development'])
     app.run()
