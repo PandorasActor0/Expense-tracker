217bf7635f8118262dd5197c9873578836ed0a28
PandorasActor0 <nelsona2001@hotmail.com>
2023-11-06 10:19:52 -0500

    the connection to the database is configured

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask,jsonify
 from config import config
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+conexion=MySQL(app)
 
 @app.route('/')
 def index():
     return "Welcome"
 
+@app.route('/expenses')
+def listar_expenses():
+    try:
+        cursor= conexion.connection.cursor()
+        sql="SELECT * FROM expenses"
+        cursor.execute(sql)
+        datos = cursor
+        expenses=[]
+        for fila in datos:
+            expense={'expense_id':fila[0],'user':fila[1],'amount':fila[2],'description':fila[3],'category_id':fila[4],'date':fila[5]}
+            expenses.append(expense)
+        return jsonify({'expenses':expenses,'mensaje':"list of expenses"})
+    except Exception as ex:
+        return jsonify({'mensaje':"Error"})
+
+def pagina_no_encontrada(error):
+    return "<h1> La pagina que intentas buscar no existe </h1>"
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
+    app.register_error_handler(404,pagina_no_encontrada)
     app.run()
--- backend/src/config.py
+++ backend/src/config.py
@@ -1,5 +1,9 @@
 class DevelopmentConfig():
     DEBUG=True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'personalexpensesdb'
 
 config = {
     'development': DevelopmentConfig
