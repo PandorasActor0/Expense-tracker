b425b7970be5d0096280c7378e1b897303484285
PandorasActor0 <nelsona2001@hotmail.com>
2023-11-06 10:41:01 -0500

    Search by user is added

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request
 from config import config
 from flask_mysqldb import MySQL
 
@@ -10,7 +10,7 @@ conexion=MySQL(app)
 def index():
     return "Welcome"
 
-@app.route('/expenses')
+@app.route('/expenses', methods=['GET'])
 def listar_expenses():
     try:
         cursor= conexion.connection.cursor()
@@ -24,6 +24,26 @@ def listar_expenses():
         return jsonify({'expenses':expenses,'mensaje':"list of expenses"})
     except Exception as ex:
         return jsonify({'mensaje':"Error"})
+
+@app.route('/expensesUser', methods=['GET'])
+def leer_expenses():
+    try:
+        args = request.args
+        user = args.get("user")
+        cursor= conexion.connection.cursor()
+        sql="SELECT * FROM expenses WHERE user = '{0}'".format(user)
+        cursor.execute(sql)
+        datos = cursor
+        expenses=[]
+        if datos != None:
+            for fila in datos:
+                expense={'expense_id':fila[0],'user':fila[1],'amount':fila[2],'description':fila[3],'category_id':fila[4],'date':fila[5]}
+                expenses.append(expense)
+            return jsonify({'expenses':expenses,'mensaje':"list of expenses"})
+    except Exception as ex:
+        return jsonify({'mensaje':"Error"})
+    
+
 
 def pagina_no_encontrada(error):
     return "<h1> La pagina que intentas buscar no existe </h1>"
